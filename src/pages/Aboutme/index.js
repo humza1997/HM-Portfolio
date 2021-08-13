10aa020dd43b1e11140a405452a92aa6d4e574cf
humza1997 <humza786@gmail.com>
2021-08-13 22:05:02 +0100

    added

--- src/pages/Aboutme/index.js
+++ src/pages/Aboutme/index.js
@@ -5,7 +5,16 @@ const Aboutme = () => {
   return (
     <div id='aboutme'>
       <h1>About Me</h1>
-      <p>Hi there, I’m Humza! I made the decision to switch careers to tech and i haven’t looked back since!</p>
+      <p>
+        Hi there, I’m Humza and I love to create!
+        <br />
+        <br />
+        In 2018, I graduated from Queen Mary University of London, with a BEng in Mechanical Engineering. Shortly after,
+        I started a graduate scheme in Building Services Engineering, working for the largest housing association in
+        London. Two years into my grad scheme, I made the decision to switch careers to tech and I haven’t looked back
+        since!
+        <br /> <br />
+      </p>
     </div>
   );
 };
